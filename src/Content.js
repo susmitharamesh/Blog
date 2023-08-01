6ecaf0b17befdd1362d6877f607ce0b46f4619f5
SUSMITHA R <susmitharamesh23@gmail.com>
2023-08-01 11:23:31 +0530

    Blog

---
+++ src/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Content=()=>
+{
+    return(
+        <div clasName='content'>
+            <div className="c1"><h2>Opening Party!</h2>
+            <p>Written By Susmitha</p></div>
+            <div className="c2">
+            <h2>Welcome to the New Blog</h2>
+            <p>Written By Susmitha Ramesh</p>
+            </div>
+            
+        </div>
+    )
+}
+export default Content
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const Navbar=()=>{
+    return(
+        <div className='Navbar'>
+            <div className='nav'>
+                <h2> Title Blog</h2>
+                <div className='btn'><button className='btn1'>Home</button>
+<button className='btn2'>NewBlog</button></div>
+
+
+</div>
+
+
+ </div>
+    )
+};
+export default Navbar;
